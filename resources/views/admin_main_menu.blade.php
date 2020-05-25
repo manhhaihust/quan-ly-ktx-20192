a69d697eb125491cc4fe53ee9a3cd2caa730748a
= <=>
2020-05-26 02:43:52 +0700

    create table user

--- resources/views/admin_main_menu.blade.php
+++ resources/views/admin_main_menu.blade.php
@@ -3,5 +3,4 @@
 	<li><a href="{{route('admin_list_cb')}}"><i class="fa fa-folder"></i><span>Danh sách cán bộ quản lý</span></a></li>
 	<li><a href="{{route('admin_info_cb')}}"><i class="fa fa-user"></i><span>Thông tin cán bộ quản lý</span></a></li>
 	<li><a href="{{route('admin_add_cb')}}"><i class="fa fa-check-square"></i><span>Tạo tài khoản quản lý</span></a></li>
-	<li><a href="{{route('admin_statics')}}"><i class="fa fa-bar-chart"></i><span>Thống kê</span></a></li>
 </ul>
